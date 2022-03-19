e3b7392e9493595dcb2e7dfc652924e71cc717c9
jmbeach <jaredbeachdesign@gmail.com>
2022-03-19 10:41:43 -0400

    adds ability to list all symbols in folder

--- src/extension.ts
+++ src/extension.ts
@@ -14,8 +14,17 @@ function processNodes(symbols: vscode.DocumentSymbol[], depth: number): string {
   return result;
 }
 
+const getRelativeFilePath = (uri: vscode.Uri) => {
+  const workspacePath = vscode.workspace.getWorkspaceFolder(uri)?.uri.path as string;
+  let folderPath = uri.path.replace(workspacePath, '');
+  if (folderPath.indexOf('/') === 0) {
+    folderPath = folderPath.substring(1);
+  }
+  return folderPath;
+};
+
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('extension.listSymbols', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.listSymbols', () => {
     if (!vscode.window.activeTextEditor) {
       vscode.window.showWarningMessage('There must be an active text editor');
       return;
@@ -28,7 +37,37 @@ export function activate(context: vscode.ExtensionContext) {
           vscode.window.showTextDocument(doc);
         });
       });
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(vscode.commands.registerCommand('extension.listAllSymbolsInFolder', (fileMeta) => {
+    const folderPath = getRelativeFilePath(fileMeta);
+
+    // TODO: Make a config so that users can choose to exclude files with a glob
+    vscode.workspace.findFiles(`${folderPath}/**`, undefined, undefined).then(uris => {
+      const promises = [];
+      for (const uri of uris) {
+        const p = new Promise<{ symbols: vscode.DocumentSymbol[], fileUri: vscode.Uri}>((resolve) => {
+          (vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', uri) as Thenable<vscode.DocumentSymbol[]>).then(symbols => {
+            resolve({
+              fileUri: uri,
+              symbols
+            });
+          });
+        });
+        promises.push(p);
+      }
+      Promise.all(promises).then(allSymbols => {
+        let fullText = '';
+        for (const fileSymbols of allSymbols) {
+          fullText += `${getRelativeFilePath(fileSymbols.fileUri)}\n---\n`;
+          fullText += processNodes(fileSymbols.symbols, 0);
+          fullText += '\n';
+        }
+
+        vscode.workspace.openTextDocument({ content: fullText }).then(doc => {
+          vscode.window.showTextDocument(doc);
+        });
+      });
+    });
+  }));
 }
